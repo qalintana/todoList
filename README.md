d16ee471dbf39e0ac7bb3163b7407b5c415abf31
qalintana <alintana18@gmail.com>
2020-06-03 17:15:35 +0100

    terminada a criacao de metodos para obtenção de ficheiros

---
+++ README.md
@@ -0,0 +1,10 @@
+# lista_tarefas
+
+Projeto que lista os apps gerenciadores de tarefa
+
+## Getting Started
+
+TA presente aplicação foi criada usando o flutter como tecnologia.
+
+A few resources to get you started if this is your first Flutter project:
+
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _todoList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = jsonEncode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+}
